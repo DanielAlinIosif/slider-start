b63c28537966001687cc757a0595b8ed4dfa1d56
DanielAlinIosif <aliniosifdaniel@gmail.com>
2024-01-25 05:17:45 +0200

    git commit -m "

---
+++ script.js
@@ -0,0 +1,34 @@
+const mainSlide = document.querySelector('.main-slide');
+const numberOfSlides = mainSlide.querySelectorAll('div').length;
+const upBtn = document.querySelector('.up-button');
+const downBtn = document.querySelector('.down-button');
+let indexActiveSlide = 0;
+
+mainSlide.style.top =  '-${(numberOfSlides - 1) * 100}vh';
+
+upBtn.addEventListener('click', () => {
+    changeSlider('up');
+});
+
+downBtn.addEventListener('click', () => {
+    changeSlider('down');
+});
+
+
+function changeSlider(direction) {
+    if(direction === 'up') {
+        indexActiveSlide++;
+
+        if(indexActiveSlide === numberOfSlides) {
+            indexActiveSlide = 0;
+        }
+    }  else if (direction === 'down') {
+        indexActiveSlide--;
+        if(indexActiveSlide < 0) {
+            indexActiveSlide = numberOfSlides - 1;
+        }
+    }
+
+    console.log(indexActiveSlide); 
+
+}
