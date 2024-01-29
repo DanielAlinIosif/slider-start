a724b047be93ee5c3ecb0513c0ece3a097f3af18
DanielAlinIosif <aliniosifdaniel@gmail.com>
2024-01-29 22:41:21 +0200

    git commit -m "

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
-const container = document.querySelector('.container');
-const sidebar = document.querySelector('.sidebar');
-const mainSlide = document.querySelector('.main-slide');
-const numberOfSlides = mainSlide.querySelectorAll('div').length;
-const upBtn = document.querySelector('.up-button');
-const downBtn = document.querySelector('.down-button');
-let indexActiveSlide = 0;
-
-mainSlide.style.top =  '-${(numberOfSlides - 1) * 100}vh';
-
-upBtn.addEventListener('click', () => {
-    changeSlider('up');
-});
-
-downBtn.addEventListener('click', () => {
-    changeSlider('down');
-});
-
-
-function changeSlider(direction) {
-    if(direction === 'up') {
-        indexActiveSlide++;
-
-        if(indexActiveSlide === numberOfSlides) {
-            indexActiveSlide = 0;
+    document.addEventListener("DOMContentLoaded", function () {
+    const container = document.querySelector(".container");
+    const sidebar = document.querySelector(".sidebar");
+    const mainSlide = document.querySelector(".main-slide");
+    const downButton = document.querySelector(".down-button");
+    const upButton = document.querySelector(".up-button");
+  
+    let activeSlideIndex = 0;
+  
+    upButton.addEventListener("click", () => {
+      changeSlide("up");
+    });
+  
+    downButton.addEventListener("click", () => {
+      changeSlide("down");
+    });
+  
+    function changeSlide(direction) {
+      if (direction === "up") {
+        activeSlideIndex++;
+        if (activeSlideIndex === mainSlide.children.length) {
+          activeSlideIndex = 0;
         }
-    }  else if (direction === 'down') {
-        indexActiveSlide--;
-        if(indexActiveSlide < 0) {
-            indexActiveSlide = numberOfSlides - 1;
+      } else if (direction === "down") {
+        activeSlideIndex--;
+        if (activeSlideIndex < 0) {
+          activeSlideIndex = mainSlide.children.length - 1;
         }
+      }
+  
+      const height = mainSlide.clientHeight;
+      mainSlide.style.transform = `translateY(${-activeSlideIndex * height}px)`;
     }
-    const height = container.clientHeight;
-
-    mainSlide.style.transform = 'translateY(${height * indexActiveSlide}px)';
-    sidebar.style.transform = 'translateY(${-height * indexActiveSlide}px)';
-}
+  });
+  
